d295b2b9d77bc3caa57eebfbf3aadd3bc62b5e50
YOGESHRAJARAM <yogiyogeshrajaram@gmail.com>
2023-08-22 17:09:12 +0530

    we get seperated number

--- atm.py
+++ atm.py
@@ -2,8 +2,11 @@ import math
 def main():
     card_no_orgi = int(input("Enter yout card number please: -- "))
     validate_count(card_no_orgi)
-    if card_number == 16:
-      print("card has 16 number")
+    if card_number == 5:
+      print("step 1 ","card has 5 number")
+      number_seperat(card_no_orgi)
+      print("step 4 after seperate",seperated_num)
+
     else:
       print("please enter valide number you gives only "+str(card_number)+" number")
 
@@ -11,6 +14,23 @@ def validate_count(a):
      global card_number
      card_number=len(str(a))
      return card_number
+     
+
+def number_seperat(a):
+    #global last_number
+    global afterlast_remove
+    global seperated_num
+    seperated_num = [int(x) for x in str(a)]
+    print("step 2 seperated number",seperated_num)
+    #last_number = seperated_num[-1]
+    afterlast_remove = seperated_num.pop(-1)
+    print("step 3 that last number",afterlast_remove)
+    
+    return seperated_num 
+    
+   
+
+
 
 
 main()
