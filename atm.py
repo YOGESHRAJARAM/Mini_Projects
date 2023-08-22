948b6ec1bf8c767c5b8f32c94592bab4b41db361
YOGESHRAJARAM <yogiyogeshrajaram@gmail.com>
2023-08-22 16:26:52 +0530

    verifiying degits length finished

---
+++ atm.py
@@ -0,0 +1,19 @@
+import math
+def main():
+    card_no_orgi = int(input("Enter yout card number please: -- "))
+    validate_count(card_no_orgi)
+    if card_number == 16:
+      print("card has 16 number")
+    else:
+      print("please enter valide number you gives only "+str(card_number)+" number")
+
+def validate_count(a):
+     global card_number
+     card_number=len(str(a))
+     return card_number
+
+
+main()
+    
+
+    
